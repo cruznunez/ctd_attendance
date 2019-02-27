f20198eef2a0db146ad128f63e935986b809a5e4
Cruz Nunez <nunez.a.cruz@gmail.com>
2019-02-26 23:26:06 -0500

    Fix lesson validation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :semester
 
-  validates :slides, uniqueness: true, allow_nil: true
+  validates :slides_name, uniqueness: true, allow_nil: true
 
   validates_presence_of :semester, :title
 end
